f6ef7a7e1831354016d7bf17d438621a3a8e7f60
wamaco <149505101+wamaco@users.noreply.github.com>
2025-03-05 21:43:27 +0800

    Add files via upload

---
+++ 03_self_balancing_trees/0830-1000/list.py
@@ -0,0 +1,16 @@
+class OrderedSet:
+    def __init__(self):
+        self.contents = []
+        super().__init__()
+
+
+    def add(self, val):
+        self.contents.append(val)
+
+
+    def remove(self, val):
+        self.contents = [v for v in self.contents if v != val]
+
+
+    def __contains__(self, val):
+        return val in self.contents
