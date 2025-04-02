24465ae908087c5d0c62441ecfcfdd34efb2892c
wamaco <149505101+wamaco@users.noreply.github.com>
2025-04-02 09:09:25 +0800

    Create 17_primality_factorization/factorize.py

---
+++ 17_primality_factorization/factorize.py
@@ -0,0 +1,15 @@
+from math import gcd
+from random import randrange
+
+def find_factor(n):
+    f = lambda x: (x**2 + 1) % n
+    a = randrange(n)
+
+    tortoise = hare = a
+    while True:
+        tortoise = f(tortoise)
+        hare = f(f(hare))
+
+        # did we cycle?
+        if (g := gcd(tortoise - hare, n)) > 1:
+            return g
